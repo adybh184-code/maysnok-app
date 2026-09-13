ec94a0379d0a2f29cc1498afb5a151f6344e81c8
adybh184-code <adybh184@gmail.com>
2026-09-13 19:26:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -86,11 +86,12 @@ def health():
     return jsonify({"status": "ok"})
 @app.get("/")
 def homepage():
-    return send_from_directory(".", "index.html") 
-   @app.get("/admin")
+    return send_from_directory(".", "index.html")
+
+@app.get("/admin")
 def admin():
     return send_from_directory(".", "admin.html")
+
 if __name__ == "__main__":
-    
     port = int(os.environ.get("PORT", 5000))
-      app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
